ca7c9de79ab0c20cbc929a9107d70e5e9a804240
lehelen19 <le.helen19@gmail.com>
2023-04-24 15:31:47 -0700

    Render book titles on authors index page

--- controllers/authors.js
+++ controllers/authors.js
@@ -2,31 +2,32 @@ const authorModel = require('../models/Author');
 const bookModel = require('../models/Book');
 
 async function index(req, res) {
-    try {
-        const authors = await authorModel.find({});
-        res.render('authors/index', { title: 'Authors', authors });
-    } catch (error) {
-        // TODO add error page
-        res.send(error);
-    }
-};
+  try {
+    const authors = await authorModel.find({}).populate('booksWritten');
+    res.render('authors/index', { title: 'Authors', authors });
+  } catch (error) {
+    // TODO add error page
+    console.log(error);
+    // res.send(error);
+  }
+}
 
 async function show(req, res) {
-    try {
-        const foundAuthor = await authorModel.findById(req.params.id);
-        const fullName = `${foundAuthor.firstName} ${foundAuthor.lastName}`;
+  try {
+    const foundAuthor = await authorModel.findById(req.params.id);
+    const fullName = `${foundAuthor.firstName} ${foundAuthor.lastName}`;
 
-        res.render('authors/show', {
-            author: foundAuthor,
-            title: fullName,
-        })
-    } catch (error) {
-        // TODO add error page
-        res.send(error);
-    }
-};
+    res.render('authors/show', {
+      author: foundAuthor,
+      title: fullName,
+    });
+  } catch (error) {
+    // TODO add error page
+    res.send(error);
+  }
+}
 
 module.exports = {
-    index,
-    show,
+  index,
+  show,
 };
