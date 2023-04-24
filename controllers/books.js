1a49d561675cb012ef33a2d2fd33bc91ef35ccae
lehelen19 <le.helen19@gmail.com>
2023-04-24 10:55:08 -0700

    Add title for books index render

--- controllers/books.js
+++ controllers/books.js
@@ -3,7 +3,7 @@ const bookModel = require('../models/Book');
 async function index(req, res) {
   try {
     const books = await bookModel.find({});
-    res.render('books/index', { books });
+    res.render('books/index', { books, title: 'Your Books' });
   } catch (err) {
     // TODO: Render error page
     res.send(err);
