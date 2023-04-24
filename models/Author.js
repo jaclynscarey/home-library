49b2f0ecc155d57dfcb5abd99b85c904da1fa9e5
jaclynscarey <jsoyama@gmail.com>
2023-04-24 13:42:22 -0400

    wip: add author model

---
+++ models/Author.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const authorSchema = new Schema ({
+    firstName: {
+        type: String,
+    },
+    lastName: {
+        type: String,
+    },
+    booksWritten: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Book'
+    }],
+});
+
+module.exports = mongoose.model('Author', authorSchema);
