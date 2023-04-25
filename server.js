5ed003f121e18cd2acbdac33984c7b46a722e48a
jaclynscarey <jsoyama@gmail.com>
2023-04-25 16:17:37 -0400

    wip: add user seesion middleware

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const logger = require('morgan');
+const session = require('express-session');
 const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
 const indexRoutes = require('./routes/index');
 const bookRoutes = require('./routes/books');
 const authorRoutes = require('./routes/authors');
@@ -16,6 +18,12 @@ app.use(logger('dev'));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
+app.use(cookieParser());
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.use('/', indexRoutes);
 app.use('/books', bookRoutes);
