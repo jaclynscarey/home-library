adba17ef6c0d2f7b8ce0d39db8182f5bf1e28c36
lehelen19 <le.helen19@gmail.com>
2023-04-25 14:38:49 -0700

    Populate user books and render

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,7 @@ const userModel = require('../models/User');
 
 async function show(req, res) {
   try {
+    await req.user.populate('booksRead');
     res.render('users/show', { title: 'User', user: req.user });
   } catch (err) {
     res.render('error', { title: 'Something Went Wrong' });
