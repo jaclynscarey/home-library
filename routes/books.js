63c2158192fa1909a4374dc4dd9e7d3bbfe526b8
lehelen19 <le.helen19@gmail.com>
2023-04-26 13:17:11 -0700

    Protect admin routes

--- routes/books.js
+++ routes/books.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const booksController = require('../controllers/books');
+const isAdmin = require('../config/isAdmin');
 
 const router = express.Router();
 
 router.get('/', booksController.index);
 
-router.post('/', booksController.create);
+router.post('/', isAdmin, booksController.create);
 
 router.get('/:id', booksController.show);
 
-router.get('/:id/edit', booksController.edit);
+router.get('/:id/edit', isAdmin, booksController.edit);
 
-router.put('/:id', booksController.update);
+router.put('/:id', isAdmin, booksController.update);
 
-router.delete('/:id', booksController.delete);
+router.delete('/:id', isAdmin, booksController.delete);
 
 module.exports = router;
