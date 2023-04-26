fc012a2408e896d0e5719892b8ceb40884b296cd
lehelen19 <le.helen19@gmail.com>
2023-04-26 11:10:53 -0700

    Write delete user book controller fn

--- controllers/users.js
+++ controllers/users.js
@@ -15,13 +15,26 @@ async function add(req, res) {
     user.booksRead.push(req.params.id);
     user.save();
     res.redirect('/books');
-
   } catch (error) {
     res.render('error', { title: 'Something Went Wrong' });
   }
-};
+}
+
+async function deleteBook(req, res) {
+  try {
+    const bookIndex = req.user.booksRead.findIndex(function (id) {
+      return id.toString() === req.params.id;
+    });
+    req.user.booksRead.splice(bookIndex, 1);
+    req.user.save();
+    res.redirect(`/users/${req.user._id}`);
+  } catch (error) {
+    res.render('error', { title: 'Something Went Wrong' });
+  }
+}
 
 module.exports = {
   show,
-  add
+  add,
+  delete: deleteBook,
 };
