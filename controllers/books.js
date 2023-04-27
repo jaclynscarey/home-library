c71d9f34a2677c1fb2a3da26ea29fb54b7d8be10
lehelen19 <le.helen19@gmail.com>
2023-04-27 14:14:21 -0700

    Refactor update controller fn

--- controllers/books.js
+++ controllers/books.js
@@ -116,6 +116,7 @@ async function deleteBook(req, res) {
         await authorModel.findByIdAndDelete(author._id);
         continue;
       }
+
       const bookIndex = author.booksWritten.findIndex(function (id) {
         return id.toString() === req.params.id;
       });
@@ -150,53 +151,15 @@ async function edit(req, res) {
 }
 
 async function update(req, res) {
-  const bookDetail = {};
-  bookDetail.title = req.body.title;
-  bookDetail.genre = req.body.genre;
-  bookDetail.publishYear = Number(req.body.publishYear);
-  bookDetail.pageCount = Number(req.body.pageCount);
-  bookDetail.author = [];
-
-  // new author names
-  const authorNames = req.body.authorName.trim().split(/\s*,\s*/);
-  const authorObjects = [];
-
-  for (let author of authorNames) {
-    if (author === '') {
-      continue;
-    }
-    const authorDetail = {};
-    const authorName = author.split(' ');
-    authorDetail.firstName = authorName[0];
-    if (authorName.length > 1) {
-      authorDetail.lastName = authorName[authorName.length - 1];
-      let middleNames = [];
-      for (let i = 1; i < authorName.length - 1; i++) {
-        middleNames.push(authorName[i]);
-      }
-      if (middleNames.length > 0) {
-        authorDetail.middleName = middleNames.join(' ');
-      }
-    }
-    authorDetail.booksWritten = [];
-    authorObjects.push(authorDetail);
-  }
-
-  // existing authors
-  let existingAuthors = req.body.existingAuthors;
-  if (typeof existingAuthors === 'string' && existingAuthors !== '') {
-    existingAuthors = existingAuthors.split();
-  }
+  const authorObjects = createNewAuthorDetails(req.body.authorName);
+  const bookDetail = createBookDetail(req.body);
+  let existingAuthors = refactorExistingAuthors(req.body.existingAuthors);
 
   try {
     const book = await bookModel.findById(req.params.id).populate('author');
     const savedAuthors = [];
 
-    // Reset book's author(s)
-    book.author = [];
-    await book.save();
-
-    // Remove book from existing authors
+    // Remove book from existing authors' booksWritten
     for (const author of book.author) {
       savedAuthors.push(author);
       const bookIndex = author.booksWritten.findIndex(function (id) {
@@ -206,7 +169,12 @@ async function update(req, res) {
       await author.save();
     }
 
-    // Add book to existing author's booksWritten & add author to book
+    // Reset book authors
+    book.author = [];
+    await book.save();
+
+    // Add book to (new) existing authors' booksWritten
+    // Add author to book
     if (existingAuthors) {
       existingAuthors.forEach(async function (authorId) {
         book.author.push(authorId);
@@ -219,7 +187,8 @@ async function update(req, res) {
       await book.save();
     }
 
-    // Create new author and add book to author's booksWritten & add author to book
+    // Create new author and add book to author's booksWritten
+    // Add author to book
     for (let authorDetail of authorObjects) {
       const author = await authorModel.create(authorDetail);
       author.booksWritten.push(book._id);
