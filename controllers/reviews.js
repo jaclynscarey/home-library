2ae4efe28c60ca4a5210396a81cf02d7bbf86f9b
lehelen19 <le.helen19@gmail.com>
2023-04-27 16:15:55 -0700

    Fix create review controller fn

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -2,18 +2,14 @@ const bookModel = require('../models/Book');
 
 async function create(req, res) {
   const book = await bookModel.findById(req.params.id);
-  const userId = req.user._id;
-  const bookDetail = {
-    ...req.body,
-    userId,
-  };
-  book.reviews.push(bookDetail);
+  req.body.user = [req.user._id];
+  book.reviews.push(req.body);
   try {
     await book.save();
+    res.redirect(`/books/${book._id}`);
   } catch {
     res.render('error', { title: 'Something Went Wrong' });
   }
-  res.redirect(`/books/${book._id}`);
 }
 
 module.exports = { create };
