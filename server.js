de609556a579e65ca8fe125aadfcfcbdb631a9cd
lehelen19 <le.helen19@gmail.com>
2023-04-28 01:16:16 -0700

    Update server

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use('*', function (req, res) {
   res.render('404', { title: '404 - Page Not Found' });
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
