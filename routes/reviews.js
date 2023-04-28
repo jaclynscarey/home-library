c20a35444d51814b35e8e84dd54114d443d78c4f
lehelen19 <le.helen19@gmail.com>
2023-04-27 23:07:49 -0700

    Protect review routes

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 const reviewsController = require('../controllers/reviews');
 
-router.post('/books/:id/reviews', reviewsController.create);
+router.post('/books/:id/reviews', ensureLoggedIn, reviewsController.create);
 
-router.delete('/books/:id/reviews', reviewsController.delete);
+router.delete('/books/:id/reviews', ensureLoggedIn, reviewsController.delete);
 
 module.exports = router;
